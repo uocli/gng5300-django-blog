693a06fd5dc9dcf10a6ae9e4a4a410506bafda08
Sean Li <cli049@uottawa.ca>
2024-09-27 21:46:24 -0400

    Added comments form

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 # blog/views.py
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by("-created_on")
@@ -24,10 +26,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
-
     return render(request, "blog/detail.html", context)
